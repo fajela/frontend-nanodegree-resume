cf710f82d940770c2ef5403c30e8541d90f42b3b
fajela <fajela@gmail.com>
2015-01-24 19:28:54 +0200

    Added job experience

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,22 +1,8 @@
-// $("#main").append("Olesia Korobka");
-// var firstName = "Olesia";
-// var age = 32;
-// var awesomeThoughts = "I am Olesia and I am awesome."
-// console.log(awesomeThoughts);
-// var email = "fajela@profirem.com.ua";
-// var newEmail = email.replace("profirem.com.ua", "gmail.com");
-// console.log(email);
-// console.log(newEmail);
-// var funThoughts = awesomeThoughts.replace("awesome", "FUN");
-// $("#main").append(funThoughts);
+
 var formattedName = HTMLheaderName.replace("%data%", "Olesia Korobka");
 var formattedRole = HTMLheaderRole.replace("%data%", "Project Manager");
 $("#header").prepend(formattedName);
 $("#header").append(formattedRole);
-
-// $("#main").append(skills);
-// $("#main").append(skills[0]);
-// $("#main").append(skills.length);
 
 var bio = {
 	"name" : "Olesia Korobka",
@@ -42,16 +28,88 @@ $("#main").append(bio.welcomeMessage);
 $("#main").append(bio.bioPic);
 
 var work = {
+	"companies" : [
+		{
+			"date" : "11/2007 - 01/2014"
+			"company" : "Ukrainian Media Holding"
+			"subdates" : [
+			{
+				"subdate" : " 10/2011 - 01/2013"
+				"position" : "Sales Director (Advertising), Argumenty i Fakty",
+				"resposibilities" : {
+					"num1" : "sales plan distribution among sales specialists, control and fulfilment;",
+					"num2" : "projects development and presentation;",
+					"num3" : "sales to and meetings with clients (to help sales specialists);",
+					"num4" : "sales department coordination."
+				}
+			},
+			{
+				"position" : "Deputy Director (Advertising), Argumenty i Fakty."
+				"subdate" : "09/2010 - 10/2011"
+				"resposibilities" : {
+					"num1" : "top agencies and clients account management;",
+					"num2" : "searching for the new clients and those who did not advertise before;",
+					"num3" : "convincing new clients, presentations and negotiations (especially to maintain pricing politics);",
+					"num4" : "relationship with present clients and agencies."
+				}
+			},
+			{
+				"position" : "Key Clients Relations Manager for 7 radio stations, Key Agencies Relationship Manager,
+Political Advertising Manager for 7 radio stations, (Advertising)"
+				"subdate" : "11/2007 - 10/2008"
+				"resposibilities" : {
+					"num1" : "key clients servicing (presentations, negotiations, conclusion of contracts, legal assistance, consulting, marketing analysis, meeting organisation, planning, execution control);",
+					"num2" : "sales to clients of the key agencies (including direct sales);",
+					"num3" : "internal financial reporting; financial analysis and statistics;",
+					"num4" : "political advertising media planning, locating, broadcast control."
+				}
+			},
+			{
+				"position" : "Deputy Director, Europa Plus, (Advertising)"
+				"subdate" : "10/2006 - 10/2007"
+				"resposibilities" : {
+					"num1" : "sales managers work and sales plan accomplishment control and support;",
+					"num2" : "internal financial reporting; financial analysis and statistics;",
+					"num3" : "searching for new sales managers, testing, interviewing;",
+					"num4" : "key clients servicing, political advertising media planning, locating, broadcast control."
+				}
+			}
+		},
+		{
+			
+		}
+		],
 	"currentPosition" : "self-employed",
 	"employer" : "business owner",
 	"yearsWorked" : "2014 up to this moment",
 	"city" : "Kiev"
+	}
+	
 };
 
 var education = {
-	"schoolName" : "International Affairs School #51",
-	"years" : "1997 - 1999",
-	"schoolCity" : "Kiev"
+	"onlineCourses" = [
+	{
+		"name" : "Coursera",
+		"specialization" : "Fundamentals of Coumputing",
+		"duration" : "for 25 weeks, finished in January 2015"
+	},
+	{
+		"name" : "Udacity",
+		"specialization" : "Front-End Web Developer (Nanodegree)",
+		"duration" : "2 months, finished in February 2015"
+	}
+	],
+	"schools" = {
+		"name" : "International Christian University",
+		"city" : "Kiev, Ukraine",
+		"degree" : "BA",
+		"major" : "Administrative Managment"
+	},
+};
+
+var project = {
+
 };
 
 $("#main").append(work["currentPosition"]);
